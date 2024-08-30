9b3e6058cf1a80672d6e270d17739f6ab2dab3e7
AdamsAliGills <116809315+AdamsAliGills@users.noreply.github.com>
2024-08-30 15:07:21 +0300

    Create kf.py

---
+++ kalman-example-kfbook/kf.py
@@ -0,0 +1,69 @@
+from filterpy.kalman import KalmanFilter
+from filterpy.common import Q_discrete_white_noise
+import math
+import numpy as np
+from numpy.random import randn
+from kf_book.mkf_internal import plot_track # plots and what not
+
+def pos_vel_filter(x, P, R, Q=0., dt=1.0):
+    """ Returns a KalmanFilter which implements a
+    constant velocity model for a state [x dx].T
+    """
+    
+    kf = KalmanFilter(dim_x=2, dim_z=1)
+    kf.x = np.array([x[0], x[1]]) # location and velocity
+    kf.F = np.array([[1., dt],
+                     [0.,  1.]])  # state transition matrix
+    kf.H = np.array([[1., 0]])    # Measurement function
+    kf.R *= R                     # measurement uncertainty
+    if np.isscalar(P):
+        kf.P *= P                 # covariance matrix 
+    else:
+        kf.P[:] = P               # [:] makes deep copy
+    if np.isscalar(Q):
+        kf.Q = Q_discrete_white_noise(dim=2, dt=dt, var=Q)
+    else:
+        kf.Q[:] = Q
+    return kf
+def compute_dog_data(z_var, process_var, count=1, dt=1.):
+    "returns track, measurements 1D ndarrays"
+    x, vel = 0., 1.
+    z_std = math.sqrt(z_var) 
+    p_std = math.sqrt(process_var)
+    xs, zs = [], []
+    for _ in range(count):
+        v = vel + (randn() * p_std)
+        x += v*dt        
+        xs.append(x)
+        zs.append(x + randn() * z_std)        
+    return np.array(xs), np.array(zs)
+
+def run(x0=(0.,0.), P=500, R=0, Q=0, dt=1.0, 
+        track=None, zs=None,
+        count=0, do_plot=True, **kwargs):
+    """
+    track is the actual position of the dog, zs are the 
+    corresponding measurements. 
+    """
+
+    # Simulate dog if no data provided. 
+    if zs is None:
+        track, zs = compute_dog_data(R, Q, count)
+
+    # create the Kalman filter
+    kf = pos_vel_filter(x0, R=R, P=P, Q=Q, dt=dt)  
+
+    # run the kalman filter and store the results
+    xs, cov = [], []
+    for z in zs:
+        kf.predict()
+        kf.update(z)
+        xs.append(kf.x)
+        cov.append(kf.P)
+
+    xs, cov = np.array(xs), np.array(cov)
+    if do_plot:
+        plot_track(xs[:, 0], track, zs, cov, **kwargs)
+    return xs, cov
+P = np.diag([500., 49.])
+Ms, Ps = run(count=50, R=10, Q=0.01, P=P)
